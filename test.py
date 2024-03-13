9b0a10b088c65df9b18341b8e57a4a060d6398df
jannisleu <jannis.leuthner99@gmail.com>
2024-03-13 16:58:10 +0100

    test script updated

--- test.py
+++ test.py
@@ -8,26 +8,25 @@ from agent import dqn_agent
 def load_model():
     model = DQN(4)
     model(tf.random.uniform(shape=(1,8)))
-    model.summary()
     model.load_weights('models/checkpoints/double_dqn_checkpoint_250')
     return model
 
 def run_env(model):
     env = gym.make("LunarLander-v2", render_mode="human")
     score = 0 
-    observation, info = env.reset()
+    observation, _ = env.reset()
 
     episode_over = False
     #while not episode_over:
-    for _ in range(6000):
+    for _ in range(3000):
         q_values = model(tf.reshape(observation, shape=(1,8)))
         greedy_action = tf.argmax(q_values, axis=1) # agent policy that uses the observation and info
         observation, reward, terminated, truncated, info = env.step(int(greedy_action))
         score += reward
 
         if terminated or truncated:
-            oberservation, info = env.reset()
-            print(score)
+            oberservation, _ = env.reset()
+            print(f'Reward: {score}')
             score = 0
 
     env.close()
