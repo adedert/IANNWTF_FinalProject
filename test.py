1154d1a7c26bec689aea0638f2ce8d3160f6785b
jannisleu <jannis.leuthner99@gmail.com>
2024-03-25 16:25:11 +0100

    updated test.py

--- test.py
+++ test.py
@@ -8,7 +8,13 @@ from agent import dqn_agent
 def load_model():
     model = DQN(4)
     model(tf.random.uniform(shape=(1,8)))
-    model.load_weights('models/checkpoints/double_dqn_checkpoint_250')
+    user_input = input("Do you want to use DQN or DDQN? Enter 1 for DQN and 2 for DDQN:")
+    if user_input == 1:
+        print('Chose DQN')
+        model.load_weights('models/checkpoints/dqn_checkpoint_250')
+    else: 
+        print('Chose DDQN')
+        model.load_weights('models/checkpoints/double_dqn_checkpoint_250')
     return model
 
 def run_env(model):
@@ -21,11 +27,11 @@ def run_env(model):
     for _ in range(3000):
         q_values = model(tf.reshape(observation, shape=(1,8)))
         greedy_action = tf.argmax(q_values, axis=1) # agent policy that uses the observation and info
-        observation, reward, terminated, truncated, info = env.step(int(greedy_action))
+        observation, reward, terminated, truncated, _ = env.step(int(greedy_action))
         score += reward
 
         if terminated or truncated:
-            oberservation, _ = env.reset()
+            observation, _ = env.reset()
             print(f'Reward: {score}')
             score = 0
 
